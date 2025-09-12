a8b1b2c56a9572f9e5382db9f15013426e79920f
KAVINESWAR-S <kavineswars770@gmail.com>
2025-09-12 10:16:44 +0530

    query-params

--- EXPRESS/src/index.mjs
+++ EXPRESS/src/index.mjs
@@ -5,13 +5,20 @@ const app=express();
 const PORT =3000;
 
 const users=[
-    {id:1,user_name:"kavineswar"},
+    {id:1,user_name:"Kavineswar"},
     {id:2,user_name:"kavin"},
     {id:3,user_name:"eswar"},
     {id:4,user_name:"kavines"},
     {id:5,user_name:"ineswar"},
 ]
 
+const Products=[
+    {id:1,product_name:"iphone 16"},
+    {id:2,product_name:"samsung s24"},
+    {id:3,product_name:"oneplus 11r"},
+    {id:4,product_name:"iphone 17"},
+    {id:5,product_name:"infinix s6"}
+]
 
 
 app.get("/",(req,res)=>{
@@ -21,8 +28,24 @@ app.get("/",(req,res)=>{
 
 
 app.get("/api/users",(req,res)=>{
+    const {query:{filter,value}}=req;
+    console.log(filter,value);
+    if(filter && value){
+        return res.send(users.filter(((user)=>user[filter].toLowerCase().includes(value))))
+    }
     res.send(users)
 })
+
+app.get("/api/products",(req,res)=>{
+    const {query:{filter,value}}=req;
+    console.log(filter,value);
+    if(filter && value){
+        return res.send(Products.filter(((product)=>product[filter].toLowerCase().includes(value))))
+    }
+    res.send(Products)
+})
+
+
 
 app.get("/api/users/:id",(req,res)=>{
     //console.log(req.params);
@@ -34,6 +57,21 @@ app.get("/api/users/:id",(req,res)=>{
     console.log(user)
     if(user){
         return res.send(user);
+    }
+    return res.status(400).send({msg:"user not found"})
+})
+
+
+app.get("/api/products/:id",(req,res)=>{
+    //console.log(req.params);
+    const id=parseInt(req.params.id);
+    if(isNaN(id)){
+       return res.status(400).send({msg:"BAD request invalid id"});
+    }
+    const product=Products.find((product)=>product.id===id)
+    console.log(product)
+    if(product){
+        return res.send(product);
     }
     return res.status(400).send({msg:"user not found"})
 })
